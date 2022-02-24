9a5a167a7bd3c61e1df1e1d7b215fdf4511ee2a9
ssar <ssar@nate.com>
2022-02-24 16:26:56 +0900

    mvc 패턴 완성

---
+++ mvc/src/main/java/site/metacoding/mvc/DispatcherServlet.java
@@ -0,0 +1,43 @@
+package site.metacoding.mvc;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import site.metacoding.mvc.config.DBConn;
+import site.metacoding.mvc.web.MyUserController;
+
+// http://localhost:8000/
+// http://localhost:8000/myuser
+// http://localhost:8000/mypost
+@WebServlet("/")
+public class DispatcherServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String domain = request.getRequestURI();
+		System.out.println("domain :"+domain);
+		
+		if(domain.equals("/myuser")) {
+			MyUserController controller = new MyUserController(request, response);
+			
+			String idStr = request.getParameter("id"); // null
+			
+			if(idStr == null) {
+				controller.getAll();
+			}else {
+				int id = Integer.parseInt(idStr);
+				controller.getOne(id);
+			}
+			
+		}
+	} // Dispatcher 스레드가 종료 (1. 클라이언트에게 연결된 response 선 짤림, 2. request 메모리영역 삭제, DB connection 반납) 
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	} // Dispatcher 스레드가 종료
+
+}
---
+++ mvc/src/main/webapp/user.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>회원정보 전체 가져오는 페이지입니다</h1>
+<hr/>
+<% for(int i=0; i<10; i++){ %>
+	
+<% } %>
+</body>
+</html>
